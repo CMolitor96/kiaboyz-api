47d9d3bfcd092f337abc48ee3ed74e3b290176ae
Charles Molitor <charlie.molitor.38@gmail.com>
2023-09-01 10:17:58 -0400

    hiding secrets

--- config/connection.js
+++ config/connection.js
@@ -2,16 +2,12 @@ require('dotenv').config();
 const mysql = require('mysql2');
 const db = mysql.createConnection(
     {
-        // host: process.env.REACT_APP_AWS_DB_HOST,
-        // user: process.env.REACT_APP_AWS_DB_USER,
-        // password: process.env.REACT_APP_AWS_DB_PASSWORD,
-        // database: process.env.REACT_APP_AWS_DB_NAME,
-        // port: process.env.REACT_APP_AWS_DB_PORT
-        host: 'kiaboyzdb.cejycvzf4iiu.us-east-2.rds.amazonaws.com',
-        user: 'CMolitor',
-        password: 'Zxcft654es',
-        database: 'kiaboyzdb',
-        port: 3306
+        host: process.env.REACT_APP_AWS_DB_HOST,
+        user: process.env.REACT_APP_AWS_DB_USER,
+        password: process.env.REACT_APP_AWS_DB_PASSWORD,
+        database: process.env.REACT_APP_AWS_DB_NAME,
+        port: process.env.REACT_APP_AWS_DB_PORT
+
     },
 );
 db.connect(function(err) {
